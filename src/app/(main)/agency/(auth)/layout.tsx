4a1fa142a2c93db5304408f7e30359e9a5e84530
Kreylanc <devjanpun@gmail.com>
2024-03-05 13:37:10 +0000

    feat: sign in/up page

---
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="h-svh flex items-center justify-center">{children}</main>
+  );
+}
---
+++ src/app/(main)/agency/layout.tsx
@@ -0,0 +1,9 @@
+import "../../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider appearance={{ baseTheme: dark }}>{children}</ClerkProvider>
+  );
+}
